a8c502da3cb836d400eb4abb9e408dc3fbc8cb95
doldenburg <d.oldenburg@born4code.pl>
2018-10-12 18:28:28 +0200

    Add components: Header, TodoForm, TodoList to app.js

--- app/app.js
+++ app/app.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import { Header } from './components/header';
+import { TodoForm } from './components/todo.form';
+import { TodoList } from './components/todo.list';
+
 import './styles/styles.scss';
 import { store } from './store';
 
@@ -9,7 +13,11 @@ import { store } from './store';
 ReactDOM.render(
   <div className="app">
     <Provider store={ store }>
-      <div className="app__container">App</div>
+      <div>
+        <Header />
+        <TodoForm />
+        <TodoList />
+      </div>
     </Provider>
   </div>,
   document.getElementById('react-app')

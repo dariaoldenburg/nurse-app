888958c9ba58439ac95482f475eecece40365f0c
doldenburg <d.oldenburg@born4code.pl>
2018-10-12 18:50:23 +0200

    Add dispatch addTodo to onSumit function

--- app/components/todo.form/todo.form.js
+++ app/components/todo.form/todo.form.js
@@ -1,40 +1,56 @@
 import React from 'react';
 import { Form, Field } from 'react-final-form';
+import { connect } from 'react-redux';
+import { func } from 'prop-types';
 
+import { todosAddItem } from '../../store/todos';
 
-const onSubmit = () => {
-  // TODO
-};
 
 const required = (value) => (value ? undefined : 'Required');
 
-const TodoForm = () => (
-  <Form
-    onSubmit={onSubmit}
-    render={({ handleSubmit, submitting, pristine, invalid }) => (
-      <form onSubmit={handleSubmit}>
-        <Field name="patientName" validate={required}>
-          {({ input, meta }) => (
-            <div>
-              <input type="text" {...input} placeholder="Name" />
-              {meta.touched && meta.error && <div>{meta.error}</div>}
-            </div>
-          )}
-        </Field>
-        <Field name="task" validate={required}>
-          {({ input, meta }) => (
-            <div>
-              <input type="text" {...input} placeholder="Task" />
-              {meta.touched && meta.error && <div>{meta.error}</div>}
-            </div>
-          )}
-        </Field>
-        <button type="submit" disabled={submitting || pristine || invalid}>
-          +
-        </button>
-      </form>
-    )}
-  />
-);
+const _TodoForm = ({ addTodo }) => {
+  const onSubmit = (values) => {
+    addTodo(values);
+  };
+
+  return (
+    <Form
+      onSubmit={onSubmit}
+      render={({ handleSubmit, submitting, pristine, invalid }) => (
+        <form onSubmit={handleSubmit}>
+          <Field name="patientName" validate={required}>
+            {({ input, meta }) => (
+              <div>
+                <input type="text" {...input} placeholder="Name" />
+                {meta.touched && meta.error && <div>{meta.error}</div>}
+              </div>
+            )}
+          </Field>
+          <Field name="task" validate={required}>
+            {({ input, meta }) => (
+              <div>
+                <input type="text" {...input} placeholder="Task" />
+                {meta.touched && meta.error && <div>{meta.error}</div>}
+              </div>
+            )}
+          </Field>
+          <button type="submit" disabled={submitting || pristine || invalid}>
+            +
+          </button>
+        </form>
+      )}
+    />
+  );
+};
+
+_TodoForm.propTypes = {
+  addTodo: func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  addTodo: (todo) => dispatch(todosAddItem(todo)),
+});
+
+const TodoForm = connect(null, mapDispatchToProps)(_TodoForm);
 
 export { TodoForm };

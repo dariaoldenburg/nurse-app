2b30c2a0a87745c2f94331cd35852ed000180340
doldenburg <d.oldenburg@born4code.pl>
2018-10-12 17:20:18 +0200

    Create component TodoList

---
+++ app/components/todo.list/todo.list.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { arrayOf, object } from 'prop-types';
+import { connect } from 'react-redux';
+
+import { TodoElement } from '../todo.element';
+
+
+const _TodoList = ({ todos }) => (
+  <div>
+    {
+      todos.map((todo) => (
+        <TodoElement key={todo.id} todo={todo} />
+      ))
+    }
+  </div>
+);
+
+_TodoList.propTypes = {
+  todos: arrayOf(object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+
+const TodoList = connect(mapStateToProps)(_TodoList);
+
+export { TodoList };
